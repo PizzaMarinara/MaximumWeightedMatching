636c5529b6a69cb23599c2be113d6d2cfe8ad100
PizzaMarinara <pizzamarinara@protonmail.com>
2022-01-18 09:43:22 +0100

    Refactoring for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,19 @@
-val junitVersion by extra { "4.13.2" }
-val junitAndroidxVersion by extra { "1.1.3" }
-plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("maven-publish")
-}
+buildscript {
+    val gradlePluginVersion by extra { "7.0.4" }
+    val kotlinVersion by extra { "1.6.10" }
 
-android {
-    defaultConfig {
-        compileSdk = 31
-        minSdk = 23
-        targetSdk = 31
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    dependencies {
+        classpath("com.android.tools.build:gradle:$gradlePluginVersion")
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
     }
 }
 
-dependencies {
-
-    // Core
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-
-    // Tests
-    testImplementation("junit:junit:$junitVersion")
-    androidTestImplementation("androidx.test.ext:junit:$junitAndroidxVersion")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("MaximumWeightedMatching") {
-            groupId = "dev.efantini"
-            artifactId = "maximumweightedmatching"
-            version = "0.1.0"
-            artifact("$buildDir/outputs/aar/$artifactId-release.aar")
-        }
-    }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ maximumweightedmatching/build.gradle.kts
@@ -0,0 +1,57 @@
+val junitVersion by extra { "4.13.2" }
+val junitAndroidxVersion by extra { "1.1.3" }
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("maven-publish")
+}
+
+android {
+    defaultConfig {
+        compileSdk = 31
+        minSdk = 23
+        targetSdk = 31
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // Core
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+
+    // Tests
+    testImplementation("junit:junit:$junitVersion")
+    androidTestImplementation("androidx.test.ext:junit:$junitAndroidxVersion")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("MaximumWeightedMatching") {
+            groupId = "dev.efantini"
+            artifactId = "maximumweightedmatching"
+            version = "0.1.2"
+            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+        }
+    }
+}
